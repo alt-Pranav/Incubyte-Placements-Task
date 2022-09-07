6284d1a4debd8792c4fbf0595d6674ff0da33dd6
alt-Pranav <74000887+alt-Pranav@users.noreply.github.com>
2022-09-07 22:55:51 +0530

    commit 1, null and empty check

---
+++ StringCalculator.java
@@ -0,0 +1,22 @@
+/**
+ *  Function Defn: int add(String numbers)
+ *  Cases:
+ *  1. Returns 0 for empty string (so 0 for null as well)
+ *  2. For a single number it will return that number
+ *  3. for input "1,2" output is 3
+ *  4. for Input: "1,2,a,c" Output: 7 (1 + 2 + 1 + 3) (only consider lowercase alphabets)
+ */
+
+public class StringCalculator{
+
+    static int add(String numbers){
+
+        int sum=0;
+
+        if(numbers == null){return sum;}
+        if(numbers == ""){return sum;}
+
+        return sum;
+    }
+
+}
---
+++ StringCalculatorTest.java
@@ -0,0 +1,25 @@
+public class StringCalculatorTest {
+
+    private final static String TEST_ONE = "NULL STRING";
+    private final static String TEST_TWO = "EMPTY STRING";
+
+    private final static String SUCCESS = " TEST was successful";
+    private final static String FAIL = " TEST has failed";
+
+    public static void main(String args[]){
+        testAdd();
+    }
+
+    static void testAdd(){
+        if(StringCalculator.add(null) == 0) {logTest(TEST_ONE, SUCCESS);}
+        else {logTest(TEST_ONE, FAIL);}
+
+        if(StringCalculator.add("") == 0) {logTest(TEST_TWO, SUCCESS);}
+        else {logTest(TEST_TWO, FAIL);}
+    }
+
+    static void logTest(String TEST, String STATE){
+        System.out.println(TEST + STATE);
+    }
+    
+}
